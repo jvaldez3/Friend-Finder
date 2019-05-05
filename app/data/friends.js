8ac8540c9c273952158f6d60934aad57c5e21da5
jval <joshvaldez3320@gmail.com>
2019-05-04 21:24:36 -0600

    page built

---
+++ app/data/friends.js
@@ -0,0 +1,91 @@
+var friends = [{
+    "name": "Regina George",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        3,
+        2,
+        3,
+        4,
+        5,
+        1,
+        1,
+        4,
+        2,
+        5
+    ]
+}, {
+    "name": "Gretchen Wieners",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        1,
+        1,
+        3,
+        5,
+        3,
+        1,
+        3,
+        4,
+        1,
+        2
+    ]
+}, {
+    "name": "Karen Smith",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        5,
+        4,
+        5,
+        3,
+        1,
+        3,
+        3,
+        2,
+        1,
+        2
+    ]
+}, {
+    "name": "Cady Heron",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        2,
+        3,
+        4,
+        2,
+        1,
+        5,
+        4,
+        5,
+        3,
+        1
+    ]
+}, {
+    "name": "Damian",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        3,
+        3,
+        3,
+        4,
+        5,
+        2,
+        3,
+        5,
+        4,
+        3
+    ]
+}, {
+    "name": "Janice Ian",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        2,
+        2,
+        3,
+        5,
+        1,
+        2,
+        3,
+        3,
+        5,
+        2
+    ]
+}]
