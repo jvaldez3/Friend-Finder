6e5bee024083703de74984ceb3bad5de8a368742
jval <joshvaldez3320@gmail.com>
2019-05-07 18:12:46 -0600

    lost

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,39 @@
 var friends = require("../data/friends.js")
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friends)
+    })
+
+    app.post("/api/friends", function (req, res) {
+        var match = {
+            name: "",
+            photo: "",
+            compare: 100
+        };
+
+        var data = req.body;
+        var friendScore = data.scores;
+        var friendName = data.name;
+        var friendPhoto = data.photo;
+
+        var scoreDiff = 0;
+
+        for (var i = 0; i < friends.length - 1; i++) {
+            console.log(friends[i].name);
+            scoreDiff = 0;
+
+            for (var j = 0; j < 10; j++) {
+                scoreDiff += Math.abs(parseInt(friendScore[j]) - parseInt(friends[i].scores[j]));
+                if (scoreDiff <= match.compare) {
+                    match.name = friends[i].name;
+                    match.photo = friends[i].photo;
+                    match.compare = scoreDiff;
+                }
+            }
+        }
+        friends.push(data);
+        res.json(match);
+    })
+}
